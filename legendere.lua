cec2580f63c6b1b34f8c528f7ab73b646e340066
VictinZero <79382302+VictinZero@users.noreply.github.com>
2024-11-25 23:29:14 +0100

    Add contents

---
+++ legendere.lua
@@ -0,0 +1,35 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: LegendereJokers
+--- MOD_ID: malverk_legendere
+--- PREFIX: malverk_legendere
+--- MOD_AUTHOR: [eaze, Victin]
+--- MOD_DESCRIPTION: Replaces the texture of the Legendary Jokers. Art by eaze. Code by Victin.
+--- VERSION: 1.0.0
+--- DEPENDENCIES: [malverk]
+
+AltTexture{
+    key = 'legendere',
+    set = 'Joker',
+    path = 'legendere.png',
+    keys = {
+        'j_caino',
+        'j_triboulet',
+        'j_yorick',
+        'j_chicot',
+        'j_perkeo'
+    },
+    soul_keys = {
+        'j_caino',
+        'j_triboulet',
+        'j_yorick',
+        'j_chicot',
+        'j_perkeo'
+    },
+}
+
+TexturePack{
+    key = 'legendere',
+    textures = {
+        'malverk_legendere_legendere'
+    }
+}
---
+++ localization/default.lua
@@ -0,0 +1,15 @@
+return {
+    descriptions = {
+        texture_packs = {
+            texpack_malverk_legendere_legendere = {
+                name = 'Legendere',
+                text = {
+                    'Retextures {C:legendary}Legendary{} Jokers',
+                    'to be {C:attention}Anime gorls{} {C:inactive}[sic]',
+                    'Art by {E:1,C:dark_edition,S:1.1}eaze',
+                    'Code by {E:1,C:dark_edition,S:1.1}Victin'
+                }
+            }
+        }
+    }
+}
